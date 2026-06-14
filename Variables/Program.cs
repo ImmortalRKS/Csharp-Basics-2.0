03446866ba264c249e312c8b721d944bc7ecc9a8
Immortal <rishitks1307@gmail.com>
2026-06-14 12:27:28 +0530

    Continued Learning

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -8,3 +8,17 @@ Console.WriteLine("a is "+ a + ", b is "+ b +", and is equal to "+ c);
 double d = 2.5;
 double e = 6.5;
 Console.WriteLine("d is " + d +", e is "+e+", and both of them is equal to "+ (d + e));
+
+bool f = true;
+Console.WriteLine("Am I Alive? " + f + " maybe lol");
+
+string name = "Rishit";
+Console.WriteLine("Hello my name is " + name);
+
+string g = name + a;
+Console.WriteLine(g);
+
+int h = 4;
+char i = '5';
+int j = h + i;
+Console.WriteLine(j);
