1fb14f97ac024a25de3965c55b02bf08e5c45e3e
ImmortalRKS <rishitks1307@gmail.com>
2026-06-17 21:50:59 +0530

    User-input 1.0

---
+++ user-input/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter your name : ");
+string a = Console.ReadLine()!;
+Console.WriteLine("Hello "+ a);
+
+
+//this is pretty basic and if you have to change the data type for eg taking age of someone from user,
+//then inspite of making another line for it to convert and to save it in a new var, you can do this instead :-
+
+Console.Write("Enter Your Age : ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your age is " + b);
+
+//this might be a bit better since now u dont need to add another var while you can get the job done in one var
+
