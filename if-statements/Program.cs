787bfd72291994b821d979995d4647ed67c1c58d
Immortal <rishitks1307@gmail.com>
2026-06-21 11:09:51 +0530

    If statements

---
+++ if-statements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Formats.Asn1;
+using System.Security.Cryptography;
+
+Console.Write("Enter your age : ");
+int a = Convert.ToInt32(Console.ReadLine())!;
+
+
+if (a == 67)
+{
+    Console.WriteLine("Ah hell nah dude, take this kid to that island ahhhh");
+}
+
+else if (a == 69)
+{
+    Console.WriteLine("I see you are a man of culture, give me some saucecode");
+}
+
+else if (a >= 18)
+{
+    Console.WriteLine("Welcome to the gang man, will do gooning in abysssssssss");
+}
+else if (a <= 0)
+{
+    Console.WriteLine("Man Atleast come with your original age, from where the heck you typing this?? inside from ur mother???");
+}
+else
+{
+    Console.WriteLine("Child go play roblox and watch tencho gaymerzz");
+}
