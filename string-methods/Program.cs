93a5d46973d833bc5bb0fd8c2104ea732fbceef1
Immortal <rishitks1307@gmail.com>
2026-06-21 10:04:20 +0530

    String Method

---
+++ string-methods/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Enter Your Name : ");
+string a = Console.ReadLine()!;
+Console.WriteLine("Your name in all capital is : " + a.ToUpper());
+Console.WriteLine("Your name in all small is :" + a.ToLower());
+
