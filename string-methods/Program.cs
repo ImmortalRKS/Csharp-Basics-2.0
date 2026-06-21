0792562e918f28b28a6949008ef3223494f315f3
Immortal <rishitks1307@gmail.com>
2026-06-21 10:24:39 +0530

    replace

--- string-methods/Program.cs
+++ string-methods/Program.cs
@@ -4,3 +4,10 @@ Console.WriteLine("Your name in all capital is : " + a.ToUpper());
 Console.WriteLine("Your name in all small is :" + a.ToLower());
 // this are only some of the basic string methods only, they are a lot of them
 
+Console.Write("Enter your phonenumber : ");
+string b = Console.ReadLine()!;
+Console.Write("Choose the number you want to replace : ");
+char c = Convert.ToChar(Console.ReadLine())!;
+Console.Write("Enter the number you want to be in the place of replaced number : ");
+char d = Convert.ToChar(Console.ReadLine())!;
+Console.WriteLine(b.Replace(c ,d));
